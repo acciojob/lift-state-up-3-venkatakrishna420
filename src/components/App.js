3466f9f8d7aaca361aa3fbe0041eccdf26793600
KAKARLA VENKATA KRISHNA <kakarlavenkatakrishna9@gmail.com>
2025-11-02 12:43:44 +0530

    Implement state for selected option in App component
    Added state management for selected option in App component.

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,28 @@
-
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
+import ChildComponent1 from "./ChildComponent1";
+import ChildComponent2 from "./ChildComponent2";
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+  const [selectedOption, setSelectedOption] = useState("");
 
-export default App
+  const handleSelect = (option) => {
+    setSelectedOption(option);
+  };
+
+  return (
+    <div className="parent">
+      {/* Do not remove the main div */}
+      <h1>Parent Component</h1>
+
+      <ChildComponent1 onSelect={handleSelect} />
+      <ChildComponent2 onSelect={handleSelect} />
+
+      <p>
+        <strong>Selected Option:</strong> {selectedOption || "None"}
+      </p>
+    </div>
+  );
+};
+
+export default App;
