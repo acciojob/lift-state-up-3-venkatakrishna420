698ec176a4b4753d4a6e1bcf199c976bdbf9c711
KAKARLA VENKATA KRISHNA <kakarlavenkatakrishna9@gmail.com>
2025-11-02 12:42:16 +0530

    Add ChildComponent1 with selection button

---
+++ src/components/ChildComponent1.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ChildComponent1 = ({ onSelect }) => {
+  return (
+    <div className="child1">
+      <h3>Child Component 1</h3>
+      <button onClick={() => onSelect("Option 1")}>Option 1</button>
+    </div>
+  );
+};
+
+export default ChildComponent1;
